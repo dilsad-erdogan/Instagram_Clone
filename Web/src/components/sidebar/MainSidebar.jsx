e91e2ac9ad4f87ef54bee8fd6f4240fe6d50a2a6
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-12-02 13:33:40 +0300

    mobile sidebar

--- Web/src/components/sidebar/MainSidebar.jsx
+++ Web/src/components/sidebar/MainSidebar.jsx
@@ -1,11 +1,8 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import Logo from "/insta_logo.png";
-import { RiHomeHeartFill, RiHomeHeartLine } from "react-icons/ri";
-import { BsSearch, BsFillSearchHeartFill } from "react-icons/bs";
-import { IoIosNotificationsOutline, IoMdNotifications } from "react-icons/io";
-import { FaPlus, FaPlusCircle, FaUserAlt, FaUserCircle } from "react-icons/fa";
 import { FiLogOut } from "react-icons/fi";
 import { logout } from "../../firebase";
+import sidebarData from "../../configData.jsx";
 
 const MainSidebar = () => {
     const navigate = useNavigate();
@@ -26,45 +23,15 @@ const MainSidebar = () => {
 
                 {/* Menu items */}
                 <div className="flex flex-col gap-10 justify-center items-start mt-10">
-                    <div className="flex items-center gap-5" onClick={() => navigate('/main')}>
-                        <div className="text-2xl">
-                            {path==="main" ? (<RiHomeHeartFill />) : (<RiHomeHeartLine />)}
+                    {sidebarData.map((data) => (
+                        <div key={data.id} className="flex items-center gap-5" onClick={() => navigate(`/${data.path}`)}>
+                            <div className="text-2xl">
+                                {path===data.path ? (data.icon1) : (data.icon2)}
+                            </div>
+
+                            <div className="text-xl">{data.path}</div>
                         </div>
-
-                        <div className="text-xl">Home</div>
-                    </div>
-
-                    <div className="flex items-center gap-5" onClick={() => navigate('/search')}>
-                        <div className="text-2xl">
-                            {path==="search" ? (<BsFillSearchHeartFill />) : (<BsSearch />)}
-                        </div>
-
-                        <div className="text-xl">Search</div>
-                    </div>
-
-                    <div className="flex items-center gap-5" onClick={() => navigate('/notification')}>
-                        <div className="text-2xl">
-                            {path==="notification" ? (<IoMdNotifications />) : (<IoIosNotificationsOutline />)}
-                        </div>
-
-                        <div className="text-xl">Notifications</div>
-                    </div>
-
-                    <div className="flex items-center gap-5" onClick={() => navigate('/create')}>
-                        <div className="text-2xl">
-                            {path==="create" ? (<FaPlusCircle />) : (<FaPlus />)}
-                        </div>
-
-                        <div className="text-xl">Create</div>
-                    </div>
-
-                    <div className="flex items-center gap-5" onClick={() => navigate('/profile')}>
-                        <div className="text-2xl">
-                            {path==="profile" ? (<FaUserCircle />) : (<FaUserAlt />)}
-                        </div>
-
-                        <div className="text-xl">Profile</div>
-                    </div>
+                    ))}
                 </div>
             </div>
 
--- Web/src/components/sidebar/SmallSidebar.jsx
+++ Web/src/components/sidebar/SmallSidebar.jsx
@@ -1,7 +1,43 @@
+import { useLocation, useNavigate } from "react-router-dom"
+import Logo from "/insta_logo.png";
+import { FiLogOut } from "react-icons/fi";
+import { logout } from "../../firebase";
+import sidebarData from "../../configData";
 
 const SmallSidebar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const path = location.pathname.split("/").filter(Boolean).pop();
+
+  const handleLogout = async () => {
+    await logout();
+    navigate('/login');
+  };
+
   return (
-    <div>SmallSidebar</div>
+    <div className="flex flex-col justify-between h-5/6">
+      {/* Top */}
+      <div>
+        {/* Logo pic */}
+        <img src={Logo} alt="Logo" className="object-cover w-full" />
+
+        {/* Menu items */}
+        <div className="flex flex-col gap-10 justify-center items-center mt-10">
+          {sidebarData.map((data) => (
+            <div key={data.id} className="flex items-center" onClick={() => navigate(`/${data.path}`)}>
+              <div className="text-2xl">
+                {path===data.path ? (data.icon1) : (data.icon2)}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Low */}
+      <div className="flex justify-center items-center p-5" onClick={handleLogout}>
+        <div className="text-2xl"><FiLogOut /></div>
+      </div>
+    </div>
   )
 }
 
