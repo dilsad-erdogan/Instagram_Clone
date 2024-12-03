16de6387d139855436dae1df67bc7d31a1af7ccd
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-12-03 20:43:36 +0300

    search user modal

---
+++ Web/src/components/modals/SearchUser.jsx
@@ -0,0 +1,20 @@
+
+const SearchUser = ({ isOpen, onClose }) => {
+    const handleClose = () => {
+        onClose();
+    };
+
+    if(!isOpen) return null;
+
+    return (
+        <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
+            <div className="bg-black rounded-lg shadow-lg p-6 w-full max-w-md">
+                <button className="absolute top-10 right-20 text-white text-3xl" onClick={handleClose}>&times;</button>
+                
+                <p>Search User</p>
+            </div>
+        </div>
+    )
+}
+
+export default SearchUser
