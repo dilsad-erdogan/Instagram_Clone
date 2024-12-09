a24704231ba1b1d58f4b36d5b17dcfb273865f86
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-12-09 19:20:02 +0300

    publish

---
+++ Mobile/src/navigation/AppNavigator.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+
+// Sayfalar
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+import Main from '../pages/Main'
+import Profile from '../pages/Profile'
+
+export type RootStackParamList = {
+    Login: undefined;
+    Register: undefined;
+    Main: undefined;
+    Profile: undefined;
+}
+
+const Stack = createStackNavigator<RootStackParamList>()
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Register" component={Register} />
+            <Stack.Screen name="Main" component={Main} />
+            <Stack.Screen name="Profile" component={Profile} />
+        </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default AppNavigator
