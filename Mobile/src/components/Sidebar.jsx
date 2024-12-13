05725249d512d56d8900b913b8d5640ff6f4e226
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-12-13 17:20:23 +0300

    logout

--- Mobile/src/components/Sidebar.jsx
+++ Mobile/src/components/Sidebar.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Feather from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { logout } from '../firebase/auth';
 
 const Sidebar = () => {
+  const navigation = useNavigation();
+
+  const handleLogout = async () => {
+    navigation.navigate('Login');
+    await logout();
+  };
+
   return (
     <View style={{ flex: 1, justifyContent: 'space-between', alignItems: 'center' }}>
       {/* Home Icon */}
-      <MaterialCommunityIcons name="home-heart-outline" size={30} color="white" />
-      
+      <TouchableOpacity onPress={() => navigation.navigate('Main')}>
+        <MaterialCommunityIcons name="home-heart-outline" size={30} color="white" />
+      </TouchableOpacity>
+
       {/* Search Icon */}
       <MaterialIcons name="search" size={30} color="white" />
       
@@ -22,10 +33,14 @@ const Sidebar = () => {
       <FontAwesome name="plus-circle" size={30} color="white" />
       
       {/* User Icon */}
-      <FontAwesome name="user" size={30} color="white" />
+      <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+        <FontAwesome name="user" size={30} color="white" />
+      </TouchableOpacity>
       
       {/* Logout Icon */}
-      <Feather name="log-out" size={30} color="white" />
+      <TouchableOpacity onPress={handleLogout}>
+        <Feather name="log-out" size={30} color="white" />
+      </TouchableOpacity>
     </View>
   );
 };
